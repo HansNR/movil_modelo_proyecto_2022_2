c77cf93d05d2d33194e31ce9a1c05f57e2cbaea0
Hans <hanswar32@gmail.com>
2022-09-28 16:29:56 -0500

    Trabajo echo por hans

--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/entity/Autor.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/entity/Autor.java
@@ -4,5 +4,76 @@ import java.util.Date;
 
 public class Autor {
 
+    private int idAutor;
+    private String nombre;
+    private String apellido;
+    private String fechanacimiento;
+    private int telefono;
+    private String fechaRegistro;
+    private int estado;
+    private Grado grado;
 
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(String fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(String fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public int getEstado() {
+        return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
+    }
+
+    public Grado getGrado() {
+        return grado;
+    }
+
+    public void setGrado(Grado grado) {
+        this.grado = grado;
+    }
 }
--- app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/registra/AutorRegistraActivity.java
+++ app/src/main/java/com/cibertec/movil_modelo_proyecto_2022_2/vista/registra/AutorRegistraActivity.java
@@ -1,21 +1,100 @@
 package com.cibertec.movil_modelo_proyecto_2022_2.vista.registra;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
 
 import com.cibertec.movil_modelo_proyecto_2022_2.R;
+import com.cibertec.movil_modelo_proyecto_2022_2.entity.Grado;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceAutor;
+import com.cibertec.movil_modelo_proyecto_2022_2.service.ServiceGrado;
+import com.cibertec.movil_modelo_proyecto_2022_2.util.ConnectionRest;
 import com.cibertec.movil_modelo_proyecto_2022_2.util.NewAppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AutorRegistraActivity extends NewAppCompatActivity {
+    Spinner spnGrado;
+    ArrayAdapter<String> adaptador;
+    ArrayList<String> grados = new ArrayList<String>();
+
+    //Servicio
+    ServiceAutor serviceAutor;
+    ServiceGrado serviceGrado;
+
+    //componentes de la GUI
+    EditText txtId, txtNom, txtApe, txtFec, txtTel,txtFre, txtestado, txtgrado;
+    Button btnRegistrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
         setContentView(R.layout.activity_autor_registra);
+
+        txtId = findViewById(R.id.txtRegEdiIdautor);
+        txtNom = findViewById(R.id.txtRegEdiNombre);
+        txtApe = findViewById(R.id.txtRegEdiApellido);
+        txtFec = findViewById(R.id.txtRegEdiFechanaciemnto);
+        txtTel = findViewById(R.id.txtRegEdiTelefono);
+        txtFre = findViewById(R.id.txtRegEdiFechaRegistro);
+        txtestado = findViewById(R.id.txtRegEdiEstado);
+        txtgrado = findViewById(R.id.txtRegEdiGrado);
+
+        btnRegistrar = findViewById(R.id.btnRegEdiEnviar);
+        serviceGrado = ConnectionRest.getConnection().create(ServiceGrado.class);
+
+        //para el adaptador
+        adaptador = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, grados);
+
+        spnGrado.setAdapter(adaptador);
+
+        cargaGrado();
+
+    }
+    public void cargaGrado(){
+        Call<List<Grado>> call = serviceGrado.Todos();
+        call.enqueue(new Callback<List<Grado>>() {
+            @Override
+            public void onResponse(Call<List<Grado>> call, Response<List<Grado>> response) {
+                if (response.isSuccessful()){
+                    List<Grado> lstGrados =  response.body();
+                    for(Grado obj: lstGrados){
+                        grados.add(obj.getIdGrado() +":"+ obj.getDescripcion());
+                    }
+                    adaptador.notifyDataSetChanged();
+                }else{
+                    mensajeToast("Error al registrar al Servicio Rest >>> ");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Grado>> call, Throwable t) {
+                mensajeToast("Error al registrar Servicio Rest >>> " + t.getMessage());
+            }
+        });
     }
 
+    public void mensajeToast(String mensaje){
+        Toast toast1 =  Toast.makeText(getApplicationContext(),mensaje, Toast.LENGTH_LONG);
+        toast1.show();
+    }
 
-
+    public void mensajeAlert(String msg){
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setMessage(msg);
+        alertDialog.setCancelable(true);
+        alertDialog.show();
+    }
 
 }
